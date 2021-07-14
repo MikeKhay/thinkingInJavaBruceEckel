ed308612c946f42671fdad918ae9c8896f0e409f
MikeKhay <khajmike@gmail.com>
2021-07-14 20:28:08 +0300

    Tasks: chapter11 (12, 14 - 25)

---
+++ src/java/chapter11/task21/CountWordInText.java
@@ -0,0 +1,25 @@
+package chapter11.task21;
+
+import java.util.*;
+
+public class CountWordInText {
+    public static void main(String[] args) {
+
+        List<String> worlds = new ArrayList<>();
+        Collections.addAll(worlds,"There are multiple ways of writing and reading a text file. this is required while dealing with many applications. There are several ways to read a plain text file in Java e.g. you can use FileReader, BufferedReader or Scanner to read a text file. Every utility provides something special e.g. BufferedReader provides buffering of data for fast reading, and Scanner provides parsing ability.We can also use both BufferReader and Scanner to read a text file line by line in Java. Then Java SE 8 introduces another Stream class java.util.stream.Stream which provides a lazy and more efficient way to read a file Note : Here usual practices of writing good code like flushing/closing streams, Exception-Handling etc, have been avoided for better understanding of codes by beginners as well.".split("\\W+"));
+
+        Map<String, Integer> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+
+        for(String s : worlds){
+            Integer freq = map.get(s);
+            map.put(s, freq == null ? 1 : freq + 1);
+        }
+
+
+
+        for(Map.Entry<String, Integer> m : map.entrySet()){
+            System.out.println("Word = " + m.getKey() + ", Count = " + m.getValue());
+        }
+
+    }
+}
---
+++ src/java/chapter11/task23/Statistics.java
@@ -0,0 +1,37 @@
+package chapter11.task23;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Statistics {
+    public static void main(String[] args) {
+
+        int countTest = 0;
+
+        while (countTest<=10) {
+            int count = 0;
+            int key = 0;
+            countTest++;
+
+            Random rand = new Random();
+
+            Map<Integer, Integer> m = new HashMap<>();
+
+            for (int i = 0; i < 1000; i++) {
+                int r = rand.nextInt(10);
+                Integer freq = m.get(r);
+                m.put(r, freq == null ? 1 : freq + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> mapCount : m.entrySet()) {
+                if (count < mapCount.getValue()) {
+                    key = mapCount.getKey();
+                    count = mapCount.getValue();
+                }
+            }
+
+            System.out.println("Test = " + countTest + ", the number is most often generated = " + key + ", Count = " + count + ", " + ( 100 * count / 1000 ) + " %.");
+        }
+    }
+}
